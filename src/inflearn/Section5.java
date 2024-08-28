6f096d09aa1e8b782a8ac8d398638e15e7cc57e8
KimChansoo <kimchan1221@naver.com>
2024-08-28 23:40:46 +0900

    24.08.28

--- src/inflearn/Section5.java
+++ src/inflearn/Section5.java
@@ -1,9 +1,6 @@
 package inflearn;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * packageName    : inflearn
@@ -97,7 +94,7 @@ public class Section5 {
     }
 
 
-    public static void main(String[] args) {
+    public static void question5_2Answer(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
@@ -114,6 +111,104 @@ public class Section5 {
         for(int i=0; i<stack.size(); i++) {
             answer += stack.get(i);
         }
+        System.out.println(answer);
+    }
+
+
+    /**
+     * 3. 크레인 인형뽑기(카카오)
+     */
+    public static void question5_3Solve(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for(int i=0; i<m; i++) {
+            moves[i] = sc.nextInt();
+        }
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<moves.length; i++) {
+            // 1 5 3 5 1 2 1 4
+            int x = moves[i];
+            for(int j=0; j<n; j++) {
+                int tmp = arr[j][x-1];
+                arr[j][x-1] = 0;    // 뽑은 인형은 0으로 초기화
+                if(tmp != 0) {
+                    if(!stack.isEmpty() && tmp == stack.peek()) {
+                        answer+=2;
+                        stack.pop();
+                    } else {
+                        stack.push(tmp);
+                    }
+                    break;
+                }
+            }
+        }
+        // 4 3 1 1 3 2 4
+        /*int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<stack.size(); i++) {
+            map.put(stack.get(i), map.getOrDefault(stack.get(i), 0)+1);
+        }
+        for(int x : map.keySet()) {
+            if(map.get(x) > 1) {
+                answer+=map.get(x);
+            }
+        }*/
+        /*int answer = 0;
+        int last = 0;
+        while(!stack.isEmpty()) {
+            int pop = stack.pop();
+            if(pop == last) {
+                answer++;
+            }
+            last = pop;
+        }*/
+
+        System.out.println(answer);
+    }
+
+
+    public static void question5_3Answer(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for(int i=0; i<m; i++) {
+            moves[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int pos : moves) {
+            for(int i=0; i<board.length; i++) {     // POINT 2차원 배열의 열 크기는 board[0].length
+                if(board[i][pos-1] != 0) {
+                    int tmp = board[i][pos-1];
+                    board[i][pos-1] = 0;        // 꺼낸 인형은 '0'(없어짐)
+                    if(!stack.isEmpty() && tmp == stack.peek()) {      // 바구니에 인형이 있을때만 비교
+                        answer+=2;
+                        stack.pop();
+                    } else {
+                        stack.push(tmp);
+                    }
+                    break;
+                }
+            }
+        }
+
         System.out.println(answer);
     }
 
